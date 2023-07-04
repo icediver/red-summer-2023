c014a70d82816a5347428b41c84d1f66063186fe
icediver <ageev75@gmail.com>
2023-07-04 17:55:26 +0300

    add dashboard menu item

--- src/components/layout/dashboard/dashboard-main-menu/DashboardMainMenu.tsx
+++ src/components/layout/dashboard/dashboard-main-menu/DashboardMainMenu.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from 'react'
 import { IDashboardMenuData, dashboardMenuData } from './data/dashvoard-menu/dashboard.menu'
 import styles from './DashboardMainMenu.module.scss'
 import clsx from 'clsx'
+import MenuItem from '../menu-item/MenuItem'
 
 const DashboardMainMenu: FC = () => {
     const [isActive, setIsActive] = useState<number>(-1)
@@ -9,27 +10,19 @@ const DashboardMainMenu: FC = () => {
         <div>
             {
                 dashboardMenuData.map((item: IDashboardMenuData, index: number) => {
-                    return <div
-                        key={item.title}
-                        className={clsx(styles.menuItem, {
-                            [styles.active]: isActive === index
-                        })}
-                    >
+                    return <MenuItem
+                        notification={item.notification}
+                        Icon={item.Icon}
+                        index={index}
+                        title={item.title}
+                        setIsActive={setIsActive}
 
-                        <button
-                            onClick={() => setIsActive(index)}>
-                            <item.Icon size={22} />
-                            <span>{item.title}</span>
-
-                        </button>
-                        {
-                            item.notification !== '' && <div className={styles.notification}>{item.notification}</div>
-
-                        }
-                    </div>
+                        isActive={
+                            isActive === index}
+                    />
                 })
             }
-        </div>
+        </div >
     </div>
 }
 
