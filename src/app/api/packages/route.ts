fb0fcd783f5d388a1c2c6816596d9c11e59fd85e
icediver <ageev75@gmail.com>
2023-08-06 22:12:47 +0300

    Update route.ts

--- src/app/api/packages/route.ts
+++ src/app/api/packages/route.ts
@@ -2,6 +2,6 @@ import { ShipmentsService } from '../services/shipments.service';
 
 export async function GET(req: Request) {
 	console.log('packages route');
-	const packages = await ShipmentsService.getAvailableParcels();
+	const packages = await ShipmentsService.getAllParcels();
 	return packages;
 }
