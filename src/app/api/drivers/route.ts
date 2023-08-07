45f6b76fdcf2cbee4626ef46d84929e217f48550
icediver <ageev75@gmail.com>
2023-08-07 09:21:22 +0300

    update api/drivers route

--- src/app/api/drivers/route.ts
+++ src/app/api/drivers/route.ts
@@ -1,9 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 import { ShipmentsService } from '../services/shipments.service';
 
 export async function GET(req: Request, res: NextApiResponse) {
 	console.log('drivers route');
-	const drivers = await ShipmentsService.getAllDrivers();
-	return drivers;
+	try {
+		const drivers = await ShipmentsService.getAllDrivers();
+		return NextResponse.json(drivers);
+	} catch (e) {
+		console.log(e);
+	}
 }
