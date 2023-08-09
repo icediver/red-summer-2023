541b9ef3443d8fc0842bbcf0e52f4adc81dc77f1
icediver <icedivet@live.ru>
2023-08-09 18:25:39 +0300

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,73 +1,70 @@
 /** @type {import('tailwindcss').Config} */
-const twColors = require('tailwindcss/colors')
+const twColors = require('tailwindcss/colors');
 
 const colors = {
-    transparent: twColors.transparent,
-    white: twColors.white,
-    black: '#484855',
-    'black-inactive': '#C2C6CF',
-    primary: '#7A57E2',
-    'primary-light': '#F2F0FC',
-    secondary: '#5AC97F',
-    'bg-color': '#FAF9FC',
-    'main-bg': '#F9F9FB',
-    'border-color': '#E9E7FA',
-    'error-color': '#F05568',
-    warning: '#F1D399',
-}
+	transparent: twColors.transparent,
+	white: twColors.white,
+	black: '#484855',
+	'black-inactive': '#C2C6CF',
+	primary: '#7A57E2',
+	'primary-light': '#F2F0FC',
+	secondary: '#5AC97F',
+	'bg-color': '#FAF9FC',
+	'main-bg': '#F9F9FB',
+	'border-color': '#E9E7FA',
+	'error-color': '#F05568',
+	warning: '#F1D399'
+};
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        colors,
-        extend: {
-            fontsize: {
-                xs: '0.82rem',
-                sm: '0.98rem',
-                base: '1.15rem',
-                lg: '1.22rem',
-                xl: '1.36rem',
-                '1.5xl': '1.5rem',
-                '2xl': '1.725rem',
-                '3xl': '2.155rem',
-                '4xl': '2.58rem',
-                '5xl': '3.45rem',
-                '6xl': '4.3rem',
-                '7xl': '5.17rem',
-                '8xl': '6.9rem',
-                '9xl': '9.2rem',
-            },
-            keyframes: {
-                animationOpacity: {
-                    from: { opacity: 0.2 },
-                    to: { opacity: 1 },
-                },
-                scaleIn: {
-                    '0%': {
-                        opacity: 0,
-                        transform: 'scale(0.9)',
-                    },
-                    '50%': {
-                        opacity: 0.3,
-                    },
-                    '100%': {
-                        opacity: 1,
-                        transform: 'scale(1)',
-                    },
-                },
-            },
-            animation: {
-                opacity: 'animationOpacity .5s easy-in-out',
-                scaleIn: 'scaleIn .35s easy-in-out',
-            },
-            gridTemplateColumns: {
-                // Простая сетка из 16 столбцов
-                '16': 'repeat(16, minmax(0, 1fr))',
-
-            }
-        },
-    },
-    plugins: [],
-}
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		colors,
+		extend: {
+			fontsize: {
+				xs: '0.82rem',
+				sm: '0.98rem',
+				base: '1.15rem',
+				lg: '1.22rem',
+				xl: '1.36rem',
+				'1.5xl': '1.5rem',
+				'2xl': '1.725rem',
+				'3xl': '2.155rem',
+				'4xl': '2.58rem',
+				'5xl': '3.45rem',
+				'6xl': '4.3rem',
+				'7xl': '5.17rem',
+				'8xl': '6.9rem',
+				'9xl': '9.2rem'
+			},
+			keyframes: {
+				animationOpacity: {
+					from: { opacity: 0.2 },
+					to: { opacity: 1 }
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)'
+					},
+					'50%': {
+						opacity: 0.3
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)'
+					}
+				}
+			},
+			animation: {
+				opacity: 'animationOpacity .5s easy-in-out',
+				scaleIn: 'scaleIn .35s easy-in-out'
+			},
+			gridTemplateColumns: {
+				// Простая сетка из 16 столбцов
+				16: 'repeat(16, minmax(0, 1fr))'
+			}
+		}
+	},
+	plugins: []
+};
